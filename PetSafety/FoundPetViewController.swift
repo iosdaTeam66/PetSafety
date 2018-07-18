8fe0e89cb8455f7bc6d142f2c09a0ea33a03d424
decristofaropaolo <decri.paolo@gmail.com>
2018-07-18 17:49:56 +0200

    cloud manager di alessandro beacon manager

--- PetSafety/FoundPetViewController.swift
+++ PetSafety/FoundPetViewController.swift
@@ -31,67 +31,62 @@ class FoundPetViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return arrayPet.count
     }
 
-    /*
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "petFoundCell", for: indexPath) as! PetEditCell
+        
+        let pet = arrayPet[indexPath.row]
+        cell.lblCellName.text = pet.name
+        cell.lblCellRace.text = pet.race
+        cell.petThumb.image = UIImage(named: pet.photo)
         return cell
+        
+        
+        /*
+        if (pet.photouuid == nil){
+            image = UIImage(named: "CatMan")
+        }
+        else {
+            let imageName = pet.photouuid // your image name here
+            let imagePath: String = "\(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0])/\(imageName!).png"
+            print (imagePath)
+            let imageUrl: URL = URL(fileURLWithPath: imagePath)
+            guard FileManager.default.fileExists(atPath: imagePath),
+                let imageData: Data = try? Data(contentsOf: imageUrl),
+                let photo = UIImage(data: imageData, scale: UIScreen.main.scale) else {
+                    print ("Immagine non trovata!")
+                    return cell
+            }
+            image = photo
+        }
+ 
+        cell.petThumb.image = image
+ */
+ 
+ 
+        
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        switch segue.identifier {
+        case "segueInfoFoundPet":
+            print("TODO SEGUE PER INFO PET TROVATO")
+        default:
+            print(#function)
+        }
     }
-    */
-
 }
