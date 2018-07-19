f6a6ee408a7ef15e11b6e1020a468b7f4fa15709
decristofaropaolo <decri.paolo@gmail.com>
2018-07-19 13:46:00 +0200

    pet found cell

---
+++ PetSafety/PetFoundCell.swift
@@ -0,0 +1,24 @@
+//
+//  PetFoundCell.swift
+//  PetSafety
+//
+//  Created by De Cristofaro Paolo on 19/07/18.
+//  Copyright © 2018 De Cristofaro Paolo. All rights reserved.
+//
+
+import UIKit
+
+class PetFoundCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
